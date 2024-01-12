f7cd68cba89de54face15116f1f462b6517778bf
nikolasperingfhdw <nikolas.pering@edu.fhdw.de>
2024-01-12 11:58:51 +0100

    1. Commit

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Ich kann programmieren!");
+        System.out.println("Aber funktioniert auch das Git Rep");
 
     }
 }
