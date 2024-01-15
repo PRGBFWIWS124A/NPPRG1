a0e485d03fb10218a75273542492cc95a87d740f
nikolasperingfhdw <156417929+nikolasperingfhdw@users.noreply.github.com>
2024-01-15 11:55:18 +0100

    Delete src directory

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.println("Ich kann programmieren!");
-        System.out.println("Aber funktioniert auch das Git Rep");
-
-    }
-}
