15473fca7aae440f1f96e8ad1c69a901aef3b927
nikolasperingfhdw <156417929+nikolasperingfhdw@users.noreply.github.com>
2024-01-15 11:54:35 +0100

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-
-        System.out.println("Ich kann programmieren!");
-
-        System.out.println("Test, ob das pushen in das Git funktioniert");
-
-    }
-}
